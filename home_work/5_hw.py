fad1a80ea9bbca1e2c2a8fdd356b2c5a5886d0bc
Natalia-137 <eiri.irbis@gmail.com>
2024-12-14 21:15:59 +0300

    Update 5_hw.py
    redone womework

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,16 +1,17 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-driver.get('https://www.saucedemo.com/')
+def find_all_elements():
+    driver = webdriver.Chrome()
+    driver.get('https://www.saucedemo.com/')
 
-name = driver.find_element(By.CSS_SELECTOR, '#user-name')
+    name = driver.find_element(By.CSS_SELECTOR, '#user-name')
 
-password = driver.find_element(By.CSS_SELECTOR, '#password')
+    password = driver.find_element(By.CSS_SELECTOR, '#password')
 
-button_submit = driver.find_element(By.CSS_SELECTOR, '#login-button')
+    button_submit = driver.find_element(By.CSS_SELECTOR, '#login-button')
 
-if not button_submit is None and not name is None and not password is None:
-    print('Элементы найдены')
-else:
-    print('Не вышло')
+    if not button_submit is None and not name is None and not password is None:
+            print('Элементы найдены')
+    else:
+            print('Не вышло')
